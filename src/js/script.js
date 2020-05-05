8785a5f74c5b4155b1bbebbd08c8cb37d2f7cfef
Sugarplum25 <sugarplamka@gmail.com>
2020-05-06 00:03:29 +0400

    finish modals

--- src/js/script.js
+++ src/js/script.js
@@ -43,9 +43,6 @@ $(document).ready(function(){
     $('.modal__close').on('click', function() {
         $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
     });
-    // $('.button_mini').on('click', function() {
-    //     $('.overlay, #order').fadeIn('slow');
-    // });
     $('.button_mini').each(function(i) {
         $(this).on('click', function() {
             $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
