bc3e4427f6175578e2d97fdcae8cb7bc2cb77fd5
Sugarplum25 <sugarplamka@gmail.com>
2020-05-07 01:21:31 +0400

    add validation

--- src/js/script.js
+++ src/js/script.js
@@ -50,3 +50,41 @@ $(document).ready(function(){
         })
     })
 });
+
+//validaton 
+
+    function validateForms (form) {
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true,
+                },
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста, введите свое имя",
+                    minlength: jQuery.validator.format("Введите не менее {0}х символов."),               
+                },
+                phone: "Пожалуйста, введите свой номер телефона",
+                email: {
+                    required: "Пожалуйста, введите свою почту",
+                    email: "Вы ввели неверную почту. Проверьте, пожалуйста, адрес."
+                },
+            },
+        });        
+    };
+
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
+    
+//TODO: очистить формы после предупреждений
+
+//mask 
+    $('input[name=phone]').mask('+7 (999) 999-99-99');
