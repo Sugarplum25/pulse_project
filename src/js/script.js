3dd2da54eec65dd8081cdc7c15a5ceecf5b3ee34
Sugarplum25 <sugarplamka@gmail.com>
2020-05-20 16:14:12 +0400

    add animation, page scroll, fix validation errors

--- src/js/script.js
+++ src/js/script.js
@@ -32,6 +32,22 @@ $(document).ready(function(){
             });
         });
     }
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                for (let q = 0; q < $(item).length; q++) {
+                    if (q === i) {
+                        continue;
+                    }
+                    $('.catalog-item__content').eq(q).addClass('catalog-item__content_active');
+                    $('.catalog-item__list').eq(q).removeClass('catalog-item__list_active');
+                }
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            });
+        });
+    }
 
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
@@ -106,5 +122,22 @@ $('form').submit(function(e) {
         $('form').trigger('reset');
     });
     return false;
-});  
+});
+
+//page up scroll
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 1600) {
+        $('.pageup').fadeIn();
+    } else {
+        $('.pageup').fadeOut();
+    }
+});
+
+$("a[href^='#']").click(function(){
+    const _href = $(this).attr("href");
+    $("html, body").animate({scrollTop: $('_href').offset().top+"px"});
+    return false;
+});
+
+
 
